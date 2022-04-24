68aef9912c7b0e6bee7f4a24cad0d00ae9439838
Enez Mutluoğlu <44931036+EnezMutluoglu@users.noreply.github.com>
2022-04-24 16:37:31 +0300

    v2

---
+++ install2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+#sudo kubeadm config images pull
+#sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=<ip> --control-plane-endpoint=<ip>
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+#kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
