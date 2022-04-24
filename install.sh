3c94cf8f367132cc8c5e56c6bd4cb8dd56e8438d
Enez Mutluoğlu <44931036+EnezMutluoglu@users.noreply.github.com>
2022-04-24 16:14:47 +0300

    v1.1

--- install.sh
+++ install.sh
@@ -3,29 +3,32 @@
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
-
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-
 sudo sysctl --system
 
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-
 sudo modprobe overlay
 sudo modprobe br_netfilter
-
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
-
 sudo sysctl --system
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install containerd -y
+sudo mkdir -p /etc/containerd
+sudo su -
+containerd config default | tee /etc/containerd/config.toml
+exit
+sudo systemctl restart containerd
 
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
